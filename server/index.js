380eb008f4bf10f61f370ba2a4ea6b495ab46f6d
Ada Rose Edwards <ada@ada.is>
2016-03-13 17:20:44 -0700

    Start on the server

--- server/index.js
+++ server/index.js
@@ -1,3 +1,25 @@
 /**
  * Do some push notifications and twitter auth
  */
+
+global.serverUrl = process.env.SERVER_URL || 'http://localhost:3000';
+const port = process.env.PORT || 3000;
+
+const express = require('express');
+const exphbs = require('express-handlebars');
+
+const app = express();
+app.engine('html', exphbs({
+	extname: '.html'
+}));
+app.set('view engine', 'html');
+
+app.get('*', express.static('build'));
+app.use(require('./twitter-auth'));
+app.use((req, res, next) => (console.log(req.user), next()));
+app.use('/', (req, res) => res.render('index', {
+	user: !!req.user
+}));
+
+app.listen(port);
+console.log('listenting on: ' + port);
---
+++ server/twitter-auth.js
@@ -0,0 +1,78 @@
+const passport = require('passport');
+const Strategy = require('passport-twitter').Strategy;
+const app = require('express')();
+const redisHost = process.env.REDIS_HOST || '127.0.0.1';
+const redisPort = Number(process.env.REDIS_PORT) || 6379;
+const appSecret = process.env.APP_SECRET || 'oh dear oh dear';
+
+
+// Configure the Twitter strategy for use by Passport.
+//
+// OAuth 1.0-based strategies require a `verify` function which receives the
+// credentials (`token` and `tokenSecret`) for accessing the Twitter API on the
+// user's behalf, along with the user's profile.	The function must invoke `cb`
+// with a user object, which will be set at `req.user` in route handlers after
+// authentication.
+passport.use(new Strategy(
+	{
+		consumerKey: process.env.CONSUMER_KEY,
+		consumerSecret: process.env.CONSUMER_SECRET,
+		callbackURL: global.serverUrl + '/auth/twitter/return'
+	},
+	function(token, tokenSecret, profile, cb) {
+		// In this example, the user's Twitter profile is supplied as the user
+		// record.	In a production-quality application, the Twitter profile should
+		// be associated with a user record in the application's database, which
+		// allows for account linking and authentication with other identity
+		// providers.
+		return cb(null, profile);
+	}
+));
+
+
+// Configure Passport authenticated session persistence.
+//
+// In order to restore authentication state across HTTP requests, Passport needs
+// to serialize users into and deserialize users out of the session.	In a
+// production-quality application, this would typically be as simple as
+// supplying the user ID when serializing, and querying the user record by ID
+// from the database when deserializing.	However, due to the fact that this
+// example does not have a database, the complete Twitter profile is serialized
+// and deserialized.
+passport.serializeUser(function(user, cb) {
+	cb(null, JSON.stringify(user));
+});
+
+passport.deserializeUser(function(obj, cb) {
+	cb(null, JSON.parse(obj));
+});
+
+// Use application-level middleware for common functionality, including
+// logging, parsing, and session handling.
+app.use(require('morgan')('combined'));
+app.use(require('cookie-parser')());
+app.use(require('body-parser').urlencoded({ extended: true }));
+app.use(require('express-session')({ secret: appSecret, resave: true, saveUninitialized: true }));
+
+// Initialize Passport and restore authentication state, if any, from the
+// session.
+app.use(passport.initialize());
+app.use(passport.session());
+
+app.get('/auth/twitter', passport.authenticate('twitter'));
+
+app.get('/auth/twitter/return',
+	passport.authenticate('twitter', { failureRedirect: '/' }),
+	function(req, res) {
+		res.redirect('/');
+	}
+);
+
+app.get('/auth/profile',
+	require('connect-ensure-login').ensureLoggedIn('/auth/twitter'),
+	function(req, res){
+		res.json('profile', { user: req.user });
+	}
+);
+
+module.exports = app;
