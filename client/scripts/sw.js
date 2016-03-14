c49853f93a7f8fc6ad375cbc5bae866b01652f28
Ada Rose <ada.edwards@ft.com>
2016-03-14 16:59:55 -0700

    lint

--- client/scripts/sw.js
+++ client/scripts/sw.js
@@ -1,10 +1,10 @@
 import * as toolbox from 'sw-toolbox';
 
 // Try network but fallback to cache
-router.default = toolbox.networkFirst;
+toolbox.router.default = toolbox.networkFirst;
 
 // Data should query the network first
-router.any('/api/*', toolbox.networkOnly);
+toolbox.router.any('/api/*', toolbox.networkOnly);
 
 // Data should query the network first
-router.any('/auth/*', toolbox.networkOnly);
+toolbox.router.any('/auth/*', toolbox.networkOnly);
