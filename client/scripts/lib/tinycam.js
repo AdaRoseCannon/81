93e57919f162ef99c27bf06d638e2ea60ed75065
Ada Rose <ada.edwards@ft.com>
2016-03-24 18:12:21 -0700

    get palette

--- client/scripts/lib/tinycam.js
+++ client/scripts/lib/tinycam.js
@@ -1,38 +1,64 @@
-/* global ColorThief */
+/* global ColorThief, $ */
+import color from 'tinycolor2';
+
 navigator.getUserMedia = navigator.getUserMedia || navigator.mozGetUserMedia || navigator.webkitGetUserMedia;
 export default () => {
+	const photoModal = $('<div class="modal"></div>');
+	document.body.prependChild(photoModal);
 	const colorThief = new ColorThief();
 	const video = document.createElement('video');
 	const canvas = document.createElement('canvas');
-	canvas.width = canvas.height = 64;
+	const buffer = document.createElement('img');
+	buffer.width = buffer.height = canvas.width = canvas.height = 64;
 	const context = canvas.getContext('2d');
 	context.imageSmoothingEnabled = false;
-	canvas.css({
-		width: 192,
-		height: 192,
-		flexShrink: 0
-	});
-	document.body.appendChild(canvas);
+	photoModal.appendChild(canvas);
+
+	let palette = false;
+
+	// sort the array into rgb objects
+	function processPalette(p) {
+		return p.map(function ([r,g,b]) {
+			return color({r,g,b});
+		})
+		.sort((a,b) => a.toHsv().v - b.toHsv().v);
+	}
+
+	function render() {
+		const h = video.videoHeight;
+		const w = video.videoWidth;
+		const smallestSide = Math.min(h, w);
+		const width = 64 * w/smallestSide;
+		const height = 64 * h/smallestSide;
+		if (isNaN(width) || isNaN(height)) return;
+		context.drawImage(video, (64 - width)/2, (64 - height)/2, width, height);
+	}
 
 	function takePhoto() {
-		context.drawImage(video, 0, 0, 64, 64);
-		// get image data from canvas
-		// assign it to html image
-		// pass it into:
-		// console.log(colorThief.getPalette(context, 16));
-		// apply palette back in canvas
-		// render it back out.
+		buffer.src = canvas.toDataURL();
+		palette = processPalette(colorThief.getPalette(buffer, 16));
+		palette.forEach(c => {
+			console.log('%c' + c.toHex(), 'background: #' + c.toHex() + ';' );
+		});
+		render();
 	}
 
 	navigator.getUserMedia({ 'video': true }, function (stream) {
-		video.src = window.URL.createObjectURL(stream);
-		video.play();
-		setTimeout(function () {
-			takePhoto();
+
+		function stop() {
 			video.pause();
 			video.src = '';
 			stream.getTracks()[0].stop();
-		}, 500);
+		}
+
+		video.src = window.URL.createObjectURL(stream);
+		video.play();
+
+		setInterval(render, 60/24);
+
+		setTimeout(function () {
+			takePhoto();
+		}, 4000);
 	}, e => {
 		console.error(e);
 	});
