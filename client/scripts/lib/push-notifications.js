4476f934fde1a1bd9bceeddab5701325abb5a723
Ada Rose Edwards <ada@ada.is>
2016-03-26 10:49:29 -0700

    better feedback

--- client/scripts/lib/push-notifications.js
+++ client/scripts/lib/push-notifications.js
@@ -2,31 +2,40 @@
 
 import swPromise from './sw.js';
 import {sendSubscriptionToServer} from './api.js';
+import {notify, warn, error} from './notify';
 
 export default function pushNotifications() {
-	const pushButton = $('#emoji__push');
-	if (pushButton) {
-		pushButton.style.display = 'none';
-		pushButton.on('click', subscribe);
+	const pushBanner = $('#emoji__push');
+	if (pushBanner) {
+		pushBanner.style.display = 'none';
+		pushBanner.on('click', subscribe);
 	}
 	function subscribe() {
+		notify('Subscribing');
+		pushBanner.classList.add('working');
+
 		swPromise.then(serviceWorkerRegistration => {
 			serviceWorkerRegistration.pushManager.subscribe({userVisibleOnly: true})
 			.then(function(subscription) {
-				pushButton.style.display = 'none';
+				pushBanner.style.display = 'none';
 				return sendSubscriptionToServer(subscription);
+				notify('Success!');
 			})
 			.catch(function(e) {
 				if (Notification.permission === 'denied') {
-					pushButton.style.display = '';
-					console.warn('Permission for Notifications was denied');
+					pushBanner.style.display = '';
+					warn('Permission for Notifications was denied');
 				} else {
 
 					// A problem occurred with the subscription; common reasons
 					// include network errors, and lacking gcm_sender_id and/or
 					// gcm_user_visible_only in the manifest.
-					console.error('Unable to subscribe to push.', e);
+					error('Unable to subscribe to push.');
+					console.log(e);
 				}
+			})
+			.then(() => {
+				pushBanner.classList.remove('working');
 			});
 		});
 	}
@@ -36,10 +45,10 @@ export default function pushNotifications() {
 	.then(subscription => {
 		if (!subscription) {
 
-			if (pushButton) {
+			if (pushBanner) {
 
 				// Not subscribed: show subscribe button
-				pushButton.style.display = '';
+				pushBanner.style.display = '';
 			}
 		} else {
 
