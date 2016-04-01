c59962e2bf219a6b377772f551d15c4d319e10cc
Ada Rose Edwards <ada@ada.is>
2016-03-31 18:38:12 -0700

    get link clicks from quotes working

--- client/scripts/quote.js
+++ client/scripts/quote.js
@@ -1,5 +1,6 @@
-/* global twemoji*/
+/* global twemoji, $$*/
 import addScript from './lib/add-script';
+import { setItem } from 'localforage';
 
 Promise.all([
 	addScript('https://cdn.rawgit.com/AdaRoseEdwards/dirty-dom/v1.3.1/build/dirty-dom-lib.min.js').promise,
@@ -7,4 +8,12 @@ Promise.all([
 	addScript('https://cdn.polyfill.io/v2/polyfill.min.js?features=fetch,default').promise
 ]).then(() => {
 	twemoji.parse(document.body);
-});
+	$$('li').forEach(li => li.on('click', function () {
+		setItem('last-correspondent', this.dataset.sender)
+		.then(function () {
+			window.location.assign('/');
+		})
+		.catch(e => console.log(e));
+	}));
+})
+.catch(e => console.log(e));
