dcbd835b71bf6c88ecc1400505b68ed3d9b995c9
Ada Rose Edwards <ada@ada.is>
2016-04-09 17:12:45 -0700

    Streamline push notification

--- client/scripts/lib/push-notifications.js
+++ client/scripts/lib/push-notifications.js
@@ -14,29 +14,28 @@ export default function pushNotifications() {
 		notify('Subscribing');
 		pushBanner.classList.add('working');
 
-		swPromise.then(serviceWorkerRegistration => {
-			serviceWorkerRegistration.pushManager.subscribe({userVisibleOnly: true})
-			.then(function(subscription) {
-				pushBanner.style.display = 'none';
-				return sendSubscriptionToServer(subscription);
-				notify('Success!');
-			})
-			.catch(function(e) {
-				if (Notification.permission === 'denied') {
-					pushBanner.style.display = '';
-					warn('Permission for Notifications was denied');
-				} else {
+		swPromise.then(
+			serviceWorkerRegistration => serviceWorkerRegistration.pushManager.subscribe({userVisibleOnly: true})
+		)
+		.then(function(subscription) {
+			pushBanner.style.display = 'none';
+			return sendSubscriptionToServer(subscription);
+		})
+		.catch(function(e) {
+			if (Notification.permission === 'denied') {
+				pushBanner.style.display = '';
+				warn('Permission for Notifications was denied');
+			} else {
 
-					// A problem occurred with the subscription; common reasons
-					// include network errors, and lacking gcm_sender_id and/or
-					// gcm_user_visible_only in the manifest.
-					error('Unable to subscribe to push.');
-					console.log(e);
-				}
-			})
-			.then(() => {
-				pushBanner.classList.remove('working');
-			});
+				// A problem occurred with the subscription; common reasons
+				// include network errors, and lacking gcm_sender_id and/or
+				// gcm_user_visible_only in the manifest.
+				error('Unable to subscribe to push.');
+				console.log(e);
+			}
+		})
+		.then(() => {
+			pushBanner.classList.remove('working');
 		});
 	}
 
